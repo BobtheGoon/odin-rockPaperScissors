a6d26a039537a7850e6e6510cb0e9d7fed521e67
Teemu <teemu9598@gmail.com>
2022-03-25 06:04:25 +0200

    Complete CLI version

--- script.js
+++ script.js
@@ -9,8 +9,25 @@ let computerSelection = computerPlay()
 let playerSelection = prompt('Choose: Rock, paper or scissors').toLowerCase();
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return 'Its a tie!'
+    if (choices.includes(playerSelection)){
+
+        playerNum = choices.indexOf(playerSelection, 0)
+        computerNum = choices.indexOf(computerSelection, 0)
+        if (playerNum == computerNum) {
+            return 'Its a tie!'
+        }
+        else if (playerNum == 0 && computerNum == 2 || playerNum == 1 && computerNum == 0 || playerNum == 2 && computerNum == 1){
+            return `You win, ${playerSelection} beats ${choices[computerNum]}!`
+        }
+
+        else {
+            return `You lose, ${computerSelection} beats ${playerSelection}`
+        }
     }
-    
+    else {
+        return 'Seems like you misstyped your selection.'
+    }
 }
+
+console.log(playerSelection +' '+ computerSelection)
+console.log(playRound(playerSelection, computerSelection))
